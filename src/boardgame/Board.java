1b8f6ed44acae4dfb5c0ff4d88b18d3d94372549
netocalegari <netokalegari@gmail.com>
2024-02-27 09:31:17 -0300

    Piece movement

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,6 +1,7 @@
 package boardgame;
 
 public class Board {
+
     private int rows;
     private int columns;
     private Piece[][] pieces;
@@ -24,13 +25,14 @@ public class Board {
 
     public Piece piece(int row, int column) {
         if (!positionExists(row, column)) {
-            throw new BoardException("Position doesn't exits");
+            throw new BoardException("Position not on the board");
         }
         return pieces[row][column];
     }
+
     public Piece piece(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Position doesn't exits");
+            throw new BoardException("Position not on the board");
         }
         return pieces[position.getRow()][position.getColumn()];
     }
@@ -43,6 +45,19 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
     private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
@@ -50,11 +65,11 @@ public class Board {
     public boolean positionExists(Position position) {
         return positionExists(position.getRow(), position.getColumn());
     }
+
     public boolean thereIsAPiece(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Position doesn't exits");
+            throw new BoardException("Position not on the board");
         }
         return piece(position) != null;
     }
-
 }
