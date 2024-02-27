648f2c21486aff2e3029a5e51eb73f45b0240784
netocalegari <netokalegari@gmail.com>
2024-02-27 14:01:16 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();
 
@@ -45,5 +45,9 @@ public class Program {
                 sc.nextLine();
             }
         }
+
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
+        sc.close();
     }
 }
